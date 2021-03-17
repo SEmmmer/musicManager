c3e03458a74c2fbb5b45d9746212d06b0adde201
ShirakamiEmmmer <emmmer@EmmmerdeMacBook-Pro.local>
2021-03-17 23:22:35 +0800

    Initial commit

---
+++ sys-ffmpeg.rb
@@ -0,0 +1,44 @@
+class Music
+  def initialize(pwd)
+    @pwd = pwd
+  end
+
+  def ffmpeg
+    info = `ffmpeg -i #{@pwd} 2>&1`
+    info.split('Metadata', 2)[1]
+  end
+
+  def artist
+    info = ffmpeg
+    info.each_line do |line|
+      return line.split(':', 2)[1].strip if line.downcase.include? 'artist'
+    end
+    nil
+  end
+
+  def title
+    info = ffmpeg
+    info.each_line do |line|
+      if line.downcase.include?('title') || line.downcase.include?('name')
+        return line.split(':', 2)[1].strip
+      end
+    end
+    nil
+  end
+
+  def album
+    info = ffmpeg
+    info.each_line do |line|
+      return line.split(':', 2)[1].strip if line.downcase.include? 'album'
+    end
+    nil
+  end
+
+  protected :ffmpeg
+end
+
+music1 = Music.new('/Users/emmmer/Downloads/a.m4p')
+# music1.ffmpeg
+puts music1.artist
+puts music1.title
+puts music1.album
